865d293da586a63edecc44b80722ca156713913c
Ugurhandasdemir <123972364+Ugurhandasdemir@users.noreply.github.com>
2024-01-03 23:07:06 +0300

    Add files via upload

---
+++ 5_sekiller_ve_metinler.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+#Şekiller ve metin
+
+#resim oluştur
+img = np.zeros((512,512,3),np.uint8) # siyah bir resim
+print(img.shape)
+
+cv.imshow("Siyah",img)
+
+#çizgi
+#(resim,başlangıc noktası, bitiş noktası, renk ,kalınlık)
+cv.line(img,(100,100),(300,300),(0,255,0),3)  #BGR
+cv.imshow("Cizgi", img)
+
+#dikdörtgen
+cv.rectangle(img, (122,122),(222,222),(255,0,0),cv.FILLED)
+cv.imshow("Diktorgen", img)
+
+#Çember
+#(resim,merkez,yarı çap, renk)
+cv.circle(img, (333,333), 20, (0,0,255),cv.FILLED)
+cv.imshow("Cember", img)
+
+#text
+#(resim,başlangıc noktası,font,kalınlığı,renk)
+cv.putText(img, "Deneme", (322,322), cv.FONT_HERSHEY_COMPLEX, 1, (255,255,255))
+cv.imshow("yazı", img)
+    
