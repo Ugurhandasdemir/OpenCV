6d118b729c79c723702656716a4602fa98d7f2d0
Ugurhandasdemir <123972364+Ugurhandasdemir@users.noreply.github.com>
2024-01-06 18:48:55 +0300

    Update 6_goruntu_birlesitrme.py

--- 6_goruntu_birlesitrme.py
+++ 6_goruntu_birlesitrme.py
@@ -1,13 +1,20 @@
 import cv2 as cv
 import numpy as np
 
+# Belirtilen yoldan resmi oku
 img = cv.imread("C:/Users/ugurh/Desktop/a.png")
+
+# Orijinal resmi göster
 cv.imshow("Orjinal", img)
 
-#yatay
-hor =np.hstack((img,img))
+# Resmin iki kopyasını yatay olarak birleştir
+hor = np.hstack((img,img))
+
+# Yatay olarak birleştirilmiş resmi göster
 cv.imshow(" yatay:",hor)
 
-#dikey
+# Resmin iki kopyasını dikey olarak birleştir
 ver = np.vstack((img,img))
+
+# Dikey olarak birleştirilmiş resmi göster
 cv.imshow("dikey:",ver)
