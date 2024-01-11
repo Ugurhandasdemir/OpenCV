1c6de8596318f8af4ff7febc953ecec128102e90
Ugurhandasdemir <123972364+Ugurhandasdemir@users.noreply.github.com>
2024-01-11 22:57:53 +0300

    Update 12_gradyanlar.py

--- 12_gradyanlar.py
+++ 12_gradyanlar.py
@@ -1,40 +1,20 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 
+# Resmi gri tonlamalı olarak oku
 img = cv.imread("C:/Users/ugurh/Documents/python/OpenCV/12-)Gradyanlar/a.png", 0)
+
+# Resmi çiz
 plt.figure(), plt.imshow(img, cmap="gray"), plt.axis("off"), plt.title("sudoku")
 
-# x gradyan
+# x gradyanını hesapla
 sobelx= cv.Sobel(img, ddepth=cv.CV_16S, dx=1, dy=0, ksize=5)
 plt.figure(), plt.imshow(sobelx, cmap="gray"), plt.axis("off"), plt.title("sobelx") 
 
-# y gradyan
+# y gradyanını hesapla
 sobely= cv.Sobel(img, ddepth=cv.CV_16S, dx=0, dy=1, ksize=5)
 plt.figure(), plt.imshow(sobely, cmap="gray"), plt.axis("off"), plt.title("sobel y")
 
-# laplacion gradyan
+# Laplasyen gradyanını hesapla
 laplacion= cv.Laplacian(img, ddepth=cv.CV_16S)
 plt.figure(), plt.imshow(laplacion, cmap="gray"), plt.axis("off"), plt.title("laplacion")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
